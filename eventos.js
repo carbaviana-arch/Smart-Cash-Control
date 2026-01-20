3da59bff64c459c25c1e899c891232da6881cf43
carbaviana-arch <carbaviana@gmail.com>
2026-01-20 13:54:51 +0100

    Delete eventos.js

--- eventos.js
+++
@@ -1,7 +0,0 @@
-// Lógica base para el módulo de Eventos
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Módulo Eventos cargado correctamente");
-
-  // Aquí se añadirán funciones para gestionar eventos y calendario
-});
