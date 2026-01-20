2ed515dc7afb52f79cdf83c1d9cc5ae1dd1f56b1
carbaviana-arch <carbaviana@gmail.com>
2026-01-20 14:02:05 +0100

    Delete personal.js

--- personal.js
+++
@@ -1,7 +0,0 @@
-// Lógica base para el módulo de Personal
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Módulo Personal cargado correctamente");
-
-  // Aquí se añadirán funciones para gestionar turnos y personal
-});
