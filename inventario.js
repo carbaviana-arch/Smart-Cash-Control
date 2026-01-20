a290a5bbc5dcb5b2cc8ff5744478a8340905fd6e
carbaviana-arch <carbaviana@gmail.com>
2026-01-20 14:00:14 +0100

    Delete inventario.js

--- inventario.js
+++
@@ -1,7 +0,0 @@
-// Lógica base para el módulo de Inventario
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Módulo Inventario cargado correctamente");
-
-  // Aquí añadiremos las funciones de gestión del inventario
-});
