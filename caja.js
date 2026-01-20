4e1ec30558b0bc37503c7fe617326586b5065f89
carbaviana-arch <carbaviana@gmail.com>
2026-01-20 13:52:47 +0100

    Delete caja.js

--- caja.js
+++
@@ -1,7 +0,0 @@
-// Lógica base para el módulo de Caja
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Módulo Caja cargado correctamente");
-
-  // Aquí se añadirán funciones para gestionar cierres de caja diarios
-});
