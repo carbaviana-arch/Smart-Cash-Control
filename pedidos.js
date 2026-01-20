66b2b9fb56b1e06cee8b5b88bd2e9f5f03ea3a68
carbaviana-arch <carbaviana@gmail.com>
2026-01-20 14:01:10 +0100

    Delete pedidos.js

--- pedidos.js
+++
@@ -1,7 +0,0 @@
-// Lógica base para el módulo de Pedidos
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Módulo Pedidos cargado correctamente");
-
-  // Aquí añadiremos las funciones para gestionar pedidos de bebidas y licores
-});
